4baf723222dd2af3df3facb30f3ee4c3300daa2b
RafaMufid <akurafamufid@gmail.com>
2025-05-07 11:38:27 +0700

    menghubungkan api/Charm ke console app

---
+++ Tubes_KPL_Program/Model/Charm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes_KPL_Program.Model
+{
+    class Charm
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public string effect { get; set; }
+    }
+}
