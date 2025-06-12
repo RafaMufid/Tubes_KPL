1bd7538033c7f0a989405a19582cec6a908a6bf9
ahmaddillan <ahmad.dillan@gmail.com>
2025-06-13 00:09:10 +0700

    membuat form GUI Battle (inprogress)

---
+++ Tubes_KPL_GUI8.0/BattleForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tubes_KPL_Libraries.Attribute;
+
+namespace Tubes_KPL_GUI8._0
+{
+    public partial class BattleForm : Form
+    {
+        public BattleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BattleForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
