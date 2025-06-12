dce1adabd840cd22401e1491e15513551be55196
ZakyAmarullah <zakym0208@gmail.com>
2025-06-13 00:35:54 +0700

    mengimplementasikan design pattern factory method

---
+++ Tubes_KPL_API/Model/GameSave.cs
@@ -0,0 +1,17 @@
+﻿using Tubes_KPL_API.Model;
+using Tubes_KPL_API.Service;
+namespace Tubes_KPL_API.Model
+{
+    public class GameSave : IGameSave
+    {
+        public GameState CreateGameState(string playerName, int idDialog, int idSave)
+        {
+            return new GameState
+            {
+                PlayerName = playerName,
+                IDDialog = idDialog,
+                IDSave = idSave
+            };
+        }
+    }
+}
