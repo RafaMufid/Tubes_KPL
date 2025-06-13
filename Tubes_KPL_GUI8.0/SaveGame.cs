3ce1575559a09009d9feeeaf0b582bf3767d352e
ZakyAmarullah <zakym0208@gmail.com>
2025-06-13 15:09:09 +0700

    membuat tampilan Game Save

---
+++ Tubes_KPL_GUI8.0/SaveGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tubes_KPL_GUI8._0
+{
+    public partial class SaveGame : Form
+    {
+        public SaveGame()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
